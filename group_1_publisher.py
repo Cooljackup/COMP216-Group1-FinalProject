b6fb471840c587eb22f6a7520c9d8eae76d2a2bc
Cooljackup <cooljackup139@gmail.com>
2026-04-12 17:57:59 -0400

    Updated Publisher. Following changes have been made to Publisher:
    Now connects with mqtt, just locally for now. Topic also just set as default for now.
    Connects to DataGenerator file I made.
    Formats the data that was also made that creates a unique uuid, timestamp, and value using the DataGenerator.
    Publisher now connects to the GUI. This allows the thread to be opened and start using the log class to use the formatted json data and print it into the textbox. GUI has not been changed.

--- group_1_publisher.py
+++ group_1_publisher.py
@@ -2,28 +2,52 @@
 
 # Import necessary packages.
 import tkinter as tk
+import paho.mqtt.client as mqtt
+import json
+import uuid
+import time
+import threading
+
+# Import DataGenerator.
+from group_1_data_generator import DataGenerator
 
 # Broker connection and Topic.
 ### TEMPORARY. Just for testing locally.
 BROKER = "127.0.0.1"
 TOPIC = "TOPIC"
 
-# Publisher class.
-### TEMPORARY. Just to have it set up.
+# Publisher class that sends the data to the broker.
 class Publisher:
     def __init__(self):
+        self.client = mqtt.Client()
+        self.generator = DataGenerator()
         self.running = False
-
-    def start(self):
+    
+    # Format data class that formats the data into a json.
+    def format_data(self, value):
+        return json.dumps({"packet_id": str(uuid.uuid4()), "timestamp": time.time(), "value": value})
+    
+    # Start class that connects to the broker and sends the formatted data as a json and stores it as a log variable.
+    def start(self, log):
+        self.client.connect(BROKER)
         self.running = True
 
+        while self.running:
+            data = self.format_data(self.generator.get_value())
+            self.client.publish(TOPIC, data)
+            log(f"Sent: {data}")
+            time.sleep(1)
+
+    # Stop class that disconnects from the broker.
     def stop(self):
+        self.client.disconnect()
         self.running = False
 
 # PublisherGUI class that creates and displays the GUI.
 class PublisherGUI:
     def __init__(self, root):
         self.root = root
+        self.publisher = Publisher()
         self.gui_setup()
 
         # To properly close the program.
@@ -40,13 +64,19 @@ class PublisherGUI:
         tk.Button(self.root, text="Start", command=self.start).pack(pady=10)
         tk.Button(self.root, text="Stop", command=self.stop).pack(pady=10)
 
-    ### TEMPORARY. Just have to set it up.
-    def start(self):
-        print("Start")
+    # Log class that stores the data into a message and inserts it into a message.
+    def log(self, message):
+        self.text.insert(tk.END, message + "\n")
 
-    ### TEMPORARY. Just have to set it up.
+    # Start class that starts publishing the data to the log on a thread.
+    def start(self):
+        self.log("Publisher has been started.")
+        threading.Thread(target=self.publisher.start, args=(self.log,)).start()
+
+    # Stop class that stops publishing data.
     def stop(self):
-        print("Stop")
+        self.publisher.stop()
+        self.log("Publisher has been stopped.")
 
 # Main class to run Publisher.
 if __name__ == "__main__":
